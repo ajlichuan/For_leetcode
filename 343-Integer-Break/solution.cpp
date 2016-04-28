05ac65d6017f32e8d39129694a33d743099a6871
ajlichuan <ajlichuan@126.com>
2016-04-28 16:19:25 +0800

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.cpp
+++ 343-Integer-Break/solution.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n < 4)
-            return n - 1;
-        else
-            return n%3 == 0 ? pow(3,n/3) : pow(3,n/3-1) * (2+2*(n%3));
+        return n < 4 ? n - 1 : n%3 == 0 ? pow(3,n/3) : pow(3,n/3-1) * (2+2*(n%3));
     }
 };
