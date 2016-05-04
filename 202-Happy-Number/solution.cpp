208c3ea0019a71042950cd3eb964be756abd6a79
ajlichuan <ajlichuan@126.com>
2016-05-04 14:58:22 +0800

    [202. Happy Number][Accepted]committed by LeetCode Extension

--- 202-Happy-Number/solution.cpp
+++ 202-Happy-Number/solution.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int> tmp;
+    int next(int n)
+    {
+        int sum = 0;
         
-        while(n != 1)
+        while(n != 0)
         {
-            if(n < 243)
-                if(tmp.find(n) == tmp.end())
-                    tmp.insert(n);
-                else
-                    return false;
-            
-            int sum = 0;
-            while(n != 0)
-            {
-                sum += pow(n % 10,2);
-                n = n / 10;
-            }
-            
-            n = sum;
+            sum += pow(n % 10,2);
+            n = n / 10;
         }
         
-        return true;
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = next(n);
+        int cnt = 1;
+        int lim = 2;
+        
+        while(slow != fast)
+        {
+            if(cnt == lim)
+            {
+                cnt = 1;
+                lim = lim*2;
+                slow = fast;
+            }
+            else
+                cnt ++;
+        
+            fast = next(fast);
+        }
+        
+        return fast == 1 ;
     }
 };
