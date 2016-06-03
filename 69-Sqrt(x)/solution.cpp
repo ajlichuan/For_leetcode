907129b20e54c0a33fa631ce1c685098717368d6
ajlichuan <ajlichuan@126.com>
2016-06-03 15:37:03 +0800

    [69. Sqrt(x)][Accepted]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.cpp
+++ 69-Sqrt(x)/solution.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        if(x < 2) return x;
-        
-        int pow = 1;
-        
-        while(pow*pow <= x/100) pow *= 10;
-        
-        int res = 0;
+	int mySqrt(int x) {
+		if (x < 2) return x;
 
-        do
-        {
-            int tmp = 0, tmpx = (x - res * res)/pow;
-            
-            while(tmp < 10 && tmp * tmp * pow + 2 * res * tmp  <= tmpx) tmp++;
-            
-            res += pow * (tmp-1);
-            
-            pow /= 10;
-            
-        }while(pow != 0);
-        
-        return res;
-    }
+		int dp = 4;
+		int pow = 0;
+
+		while (1 << (pow * 2) <= x >> (2 * dp)) pow = pow + dp;
+
+		int res = 0;
+
+		do
+		{
+			int tmp = 0, tmpx = (x - res * res) >> pow;
+
+			while (tmp < (1 << dp) && ((tmp * tmp) << pow) + 2 * res * tmp <= tmpx) tmp++;
+
+			res += (tmp - 1) << pow;
+
+			pow -= 4;
+
+		} while (pow >= 0);
+
+		return res;
+	}
 };
