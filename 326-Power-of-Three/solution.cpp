dfa5cbccb11bc106a5b91f2361b545b576a22abc
ajlichuan <ajlichuan@126.com>
2016-06-21 18:25:17 +0800

    [326. Power of Three][Accepted]committed by LeetCode Extension

--- 326-Power-of-Three/solution.cpp
+++ 326-Power-of-Three/solution.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return (n > 0) && (1162261467 / n == 1162261467.0 / n);
+        if(n == 0) return false;
+        //return (n > 0) && (1162261467 / n == 1162261467.0 / n);
+        while(n / 3.0 == n / 3)
+            n = n / 3;
+        
+        return n == 1;
     }
 };
