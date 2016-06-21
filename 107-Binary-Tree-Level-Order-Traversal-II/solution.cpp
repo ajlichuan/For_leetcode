00aadaada40442cb1751afed6d8303bc58b01e97
ajlichuan <ajlichuan@126.com>
2016-06-21 21:30:37 +0800

    [107. Binary Tree Level Order Traversal II][Accepted]committed by LeetCode Extension

--- 107-Binary-Tree-Level-Order-Traversal-II/solution.cpp
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.cpp
@@ -11,31 +11,26 @@ class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
         vector<vector<int>> res;
-        vector<int> tmp;
-        queue<TreeNode*> q;
+        queue<pair<TreeNode*,int>> q;
         
         if(root == NULL) return res;
-        q.push(root);
-        q.push(NULL);
+        
+        q.push(make_pair(root,0));
         
         while(!q.empty())
         {
-            TreeNode* tt = q.front();
-            if(tt == NULL)
-            {
-                res.push_back(tmp);
-                if(q.size() != 1)
-                    q.push(NULL);
-                tmp.clear();
-            }
-            else
-            {
-                tmp.push_back(tt->val);
-                if(tt->left != NULL)
-                    q.push(tt->left);
-                if(tt->right != NULL)
-                    q.push(tt->right);
-            }
+            TreeNode* tt = q.front().first;
+            int level = q.front().second;
+            
+            if(level+1 > res.size())
+                res.push_back(vector<int>());
+
+            res[level].push_back(tt->val);
+            if(tt->left != NULL)
+                q.push(make_pair(tt->left,level+1));
+            if(tt->right != NULL)
+                q.push(make_pair(tt->right,level+1));
+
             q.pop();
         }
         
