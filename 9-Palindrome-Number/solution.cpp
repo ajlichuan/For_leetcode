3e34f04cf15764627909a58dbb3f205db1766151
ajlichuan <ajlichuan@126.com>
2016-06-22 15:28:34 +0800

    [9. Palindrome Number][Accepted]committed by LeetCode Extension

--- 9-Palindrome-Number/solution.cpp
+++ 9-Palindrome-Number/solution.cpp
@@ -4,7 +4,7 @@ public:
         if(x < 0) return false;
         if(x < 10) return true;
         
-        int pown = 1;
+        /*int pown = 1;
         
         while(x/10 >= pown) pown *= 10;
         
@@ -16,7 +16,21 @@ public:
             x /= 10;
             x -= x/pown*pown;
             pown /= 10;
+        }*/
+        
+        int num[10];
+        int cnt = 0;
+        
+        while(x != 0)
+        {
+            int tmp = x/10;
+            num[cnt++] = x-tmp*10;
+            x = tmp;
         }
+        
+        for(int i = 0; i<cnt/2; i++)
+            if(num[i] != num[cnt-1-i])
+                return false;
         
         return true;
     }
