280bfc23120a6d154848fee80d2bc203f8b5230c
ajlichuan <ajlichuan@126.com>
2016-06-22 15:46:36 +0800

    [9. Palindrome Number][Accepted]committed by LeetCode Extension

--- 9-Palindrome-Number/solution.cpp
+++ 9-Palindrome-Number/solution.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        if(x < 10) return true;
+        if(x == 0) return true;
+        if(x < 0 || x%10 == 0) return false;
         
+        int rev = 0;
+        
+        while(x>rev)
+        {
+            rev = rev*10 + x%10;
+            x /= 10;
+        }
+        
+        return rev == x || rev/10 == x;
         /*int pown = 1;
         
         while(x/10 >= pown) pown *= 10;
@@ -18,7 +27,7 @@ public:
             pown /= 10;
         }*/
         
-        int num[10];
+        /*int num[10];
         int cnt = 0;
         
         while(x != 0)
@@ -31,6 +40,6 @@ public:
             if(num[i] != num[cnt-1-i])
                 return false;
         
-        return true;
+        return true;*/
     }
 };
