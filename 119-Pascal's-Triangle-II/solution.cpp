14c89bcff779680939ba8c83044ba8fea992fd9c
ajlichuan <ajlichuan@126.com>
2016-06-22 15:02:13 +0800

    [119. Pascal's Triangle II][Accepted]committed by LeetCode Extension

--- 119-Pascal's-Triangle-II/solution.cpp
+++ 119-Pascal's-Triangle-II/solution.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> res;
+        if(rowIndex < 0) return vector<int>();
         
-        if(rowIndex < 0) return res;
-        
-        res.push_back(1);
+        vector<int> res(rowIndex+1,1);
         
         for(int i = 0; i < rowIndex; i++)
         {
-            res.push_back(1);
-            int tmp = 1;
-            
-            for(int j = 1; j < i+1; j++)
+            for(int j = i; j > 0; j--)
             {
-                res[j] += tmp;
-                tmp = res[j] - tmp;
+                res[j] += res[j-1];
             }
         }
         
