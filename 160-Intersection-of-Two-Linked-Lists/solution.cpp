d86a454771c25637542687a74a7cce4ec5fa6579
ajlichuan <ajlichuan@126.com>
2016-06-22 23:58:53 +0800

    [160. Intersection of Two Linked Lists][Accepted]committed by LeetCode Extension

--- 160-Intersection-of-Two-Linked-Lists/solution.cpp
+++ 160-Intersection-of-Two-Linked-Lists/solution.cpp
@@ -11,17 +11,10 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *p1 = headA, *p2 = headB;
         
-        if(p1 == NULL || p2 == NULL) return NULL;
-        
         while(p1 != p2)
         {
-            p1 = p1->next;
-            p2 = p2->next;
-            
-            if(p1 == p2) return p1;
-            
-            if(p1 == NULL) p1 = headB;
-            if(p2 == NULL) p2 = headA;
+            p1 = p1 == NULL ? headB : p1->next;
+            p2 = p2 == NULL ? headA : p2->next;
         }
         
         return p1;
