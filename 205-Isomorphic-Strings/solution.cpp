add34a06be04457d145c97d4904021bf2aa831eb
ajlichuan <ajlichuan@126.com>
2016-06-22 19:17:44 +0800

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

---
+++ 205-Isomorphic-Strings/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        char slist[127];
+        char tlist[127];
+        
+        memset(slist, -1, sizeof(slist));
+        memset(tlist, -1, sizeof(tlist));
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(slist[s[i]] == -1 || tlist[t[i]] == -1)
+            {
+                if(slist[s[i]] == tlist[t[i]])
+                {
+                    slist[s[i]] = t[i];
+                    tlist[t[i]] = s[i];
+                }
+                else
+                    return false;
+            }
+            if(slist[s[i]] != t[i] || tlist[t[i]] != s[i])
+                return false;
+        }
+        
+        return true;
+    }
+};
