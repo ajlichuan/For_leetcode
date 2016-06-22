f46622345ae1e95ef2b4c92d34abfbce35b0696c
ajlichuan <ajlichuan@126.com>
2016-06-23 00:01:13 +0800

    [160. Intersection of Two Linked Lists][Accepted]committed by LeetCode Extension

--- 160-Intersection-of-Two-Linked-Lists/solution.cpp
+++ 160-Intersection-of-Two-Linked-Lists/solution.cpp
@@ -13,8 +13,8 @@ public:
         
         while(p1 != p2)
         {
-            p1 = p1 == NULL ? headB : p1->next;
-            p2 = p2 == NULL ? headA : p2->next;
+            p1 = p1==NULL ? headB : p1->next;
+            p2 = p2==NULL ? headA : p2->next;
         }
         
         return p1;
