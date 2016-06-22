8867941981f8806555e03b74c0106ed451557246
ajlichuan <ajlichuan@126.com>
2016-06-22 11:19:30 +0800

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

--- 118-Pascal's-Triangle/solution.cpp
+++ 118-Pascal's-Triangle/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        vector<int> row;
         
         for(int i = 0; i < numRows; i++)
         {
+            vector<int> row;
             if(i == 0)
             {
                 row.push_back(1);
                 res.push_back(row);
-                row.clear();
+                //row.clear();
             }
             else
             {
@@ -22,7 +22,7 @@ public:
                         row.push_back(res[i-1][j-1]+res[i-1][j]);
                 }
                 res.push_back(row);
-                row.clear();
+                //row.clear();
             }
         }
         
