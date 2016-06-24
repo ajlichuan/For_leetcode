859e7921af4499b73831b54e88ebd086521edebf
ajlichuan <ajlichuan@126.com>
2016-06-24 15:17:13 +0800

    [67. Add Binary][Accepted]committed by LeetCode Extension

--- 67-Add-Binary/solution.cpp
+++ 67-Add-Binary/solution.cpp
@@ -9,7 +9,7 @@ public:
         {
             int ia = a.size()-1 - i;
             int ib = b.size()-1 - i;
-            if(a[ia] == '0')
+            /*if(a[ia] == '0')
             {
                 if(b[ib] == '0')
                 {
@@ -45,10 +45,15 @@ public:
                         pls = true;
                     }
                 }
-            }
+            }*/
+            int tmp = a[ia] + b[ib] - '0'*2;
+            if(pls) tmp++;
+            pls = tmp > 1;
+            a[ia] = tmp - ((tmp >> 1) << 1) + '0';
         }
         
         int ia = a.size()-1 - i;
+        
         while(pls && ia >= 0)
         {
             if(a[ia] == '0')
