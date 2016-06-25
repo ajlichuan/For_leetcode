459bf62b3b2d15d67685d49ce720f0d3e29605fe
ajlichuan <ajlichuan@126.com>
2016-06-25 18:07:06 +0800

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        if(n < 3) return 0;
+        
         bool isPrime[n];
         int cnt;
         
@@ -10,14 +12,14 @@ public:
         for(int i = 2; i*i<n; i++)
         {
             if(!isPrime[i]) continue;
-            for(int j = i; j*i<n; j++)
+            for(int j = i; j*i<n; j=j+2)
             {
                 isPrime[i*j] = false;
             }
         }
         
-        cnt = 0;
-        for(int i = 2; i < n; i++)
+        cnt = 1;
+        for(int i = 3; i < n; i=i+2)
             if(isPrime[i]) cnt++;
         
         return cnt;
