8664e84f5ea20915a96fbcf36fe304bd273ec5bb
ajlichuan <ajlichuan@126.com>
2016-06-28 21:22:30 +0800

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.cpp
+++ 46-Permutations/solution.cpp
@@ -3,49 +3,28 @@ public:
     int len;
     vector<vector<int>> res;
     
-    void helper(int cnt, vector<int>&nums, vector<int>& data, bool is_used[])
+    void helper(int cnt, vector<int>&nums)
     {
-        if(cnt == 1)
+        if(cnt == len-1)
         {
-            for(int i = 0; i < len; i++)
-            {
-                if(!is_used[i])
-                {
-                    data.push_back(nums[i]);
-                    res.push_back(data);
-                    break;
-                }
-            }
+            res.push_back(nums);
         }
         else
         {
-            for(int i = 0; i < len; i++)
+            for(int i = cnt; i < len; i++)
             {
-                if(!is_used[i])
-                {
-                    bool tmp_used[len];
-                    vector<int> tdata(data);
-                    memcpy(tmp_used, is_used, sizeof(tmp_used));
-                    
-                    tdata.push_back(nums[i]);
-                    tmp_used[i] = true;
-                    helper(cnt-1, nums, tdata, tmp_used);
-                }
+                swap(nums[cnt],nums[i]);
+                helper(cnt+1, nums);
+                swap(nums[i],nums[cnt]);
             }
-            
         }
     }
     
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         len = nums.size();
-        
-        bool is_used[len];
-        for(int i = 0; i < len; i++)
-            is_used[i] = false;
-        vector<int> tmp;
-        
-        helper(len, nums, tmp, is_used);
+
+        helper(0, nums);
         
         return res;
     }
