28d63bfa22406bd12a71dda4b01785610057c952
ajlichuan <ajlichuan@126.com>
2016-07-06 00:26:36 +0800

    [313. Super Ugly Number][Accepted]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.cpp
+++ 313-Super-Ugly-Number/solution.cpp
@@ -4,14 +4,15 @@ public:
         int plen = primes.size();
         int cmp[plen];
         int pos[plen];
-        vector<int> st{1};
-        n--;
+        int st[n];
+        int cnt = 1;
         
         memset(pos, 0, sizeof(pos));
+        st[0] = 1;
         for(int i = 0; i < plen; i++)
             cmp[i] = primes[i];
         
-        while(n>0)
+        while(n > cnt)
         {
             int tmp = 0;
             for(int i = 1; i < plen; i++)
@@ -19,8 +20,7 @@ public:
                 if(cmp[tmp] > cmp[i])
                     tmp = i;
             }
-            st.push_back(cmp[tmp]);
-            n--;
+            st[cnt++] = cmp[tmp];
             
             int i=0;
 			while (i != plen)
@@ -34,6 +34,6 @@ public:
 			}
         }
         
-        return st.back();
+        return st[--cnt];
     }
 };
