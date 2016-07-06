ea2ade6d636295e93b83dc05948c867fdd1f92eb
ajlichuan <ajlichuan@126.com>
2016-07-06 15:08:07 +0800

    [147. Insertion Sort List][Accepted]committed by LeetCode Extension

--- 147-Insertion-Sort-List/solution.cpp
+++ 147-Insertion-Sort-List/solution.cpp
@@ -11,7 +11,7 @@ public:
     ListNode* insertionSortList(ListNode* head) {
         if(NULL == head || NULL == head->next) return head;
         
-        ListNode **p, *to_sort = head->next, *t1, *t2;
+        ListNode **p, *to_sort = head->next, *t;
         head->next = NULL;
         while(to_sort != NULL)
         {
@@ -20,11 +20,10 @@ public:
             while((*p) != NULL && (*p)->val < to_sort->val)
                 p = &((*p)->next);
             
-            t1 = *p;
+            t = to_sort->next;
+            to_sort->next = *p;
             *p = to_sort;
-            t2 = to_sort->next;
-            to_sort->next = t1;
-            to_sort = t2;
+            to_sort = t;
         }
         
         return head;
