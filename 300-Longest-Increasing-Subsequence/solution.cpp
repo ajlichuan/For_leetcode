4ff6d83309c40b9f135edcee16ab9970ff06b56a
ajlichuan <ajlichuan@126.com>
2016-07-06 22:32:28 +0800

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.cpp
+++ 300-Longest-Increasing-Subsequence/solution.cpp
@@ -1,25 +1,37 @@
 class Solution {
+private:
+    int find(int e, int tar, int lis[])
+    {
+        if(lis[e] < tar)
+            return e+1;
+        int mid, s = 0;
+        while(s<e)
+        {
+            mid = s + (e-s)/2;
+            if(lis[mid] < tar)
+                s = mid+1;
+            else
+                e = mid;
+        }
+        return s;
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int max = 0;
         int len = nums.size();
         if(len < 2) return len;
         
-        int lis[len] = {0};
+        int lis[len];
         
-        for(int i = 0; i < len; i++)
+        lis[0] = nums[0];
+        
+        for(int i = 1; i < len; i++)
         {
-            int tmax = 0;
-            for(int j = 0; j < i; j++)
-            {
-                if(nums[i] > nums[j] && tmax < lis[j])
-                    tmax = lis[j];
-            }
-            lis[i] = tmax+1;
-            if(tmax+1 > max)
-                max = tmax+1;
+            int pos = find(max,nums[i],lis);
+            lis[pos] = nums[i];
+            if(pos > max) max=pos;
         }
         
-        return max;
+        return max+1;
     }
 };
