68be9b3dc0bfc4ee0076703840380b038cf81971
ajlichuan <ajlichuan@126.com>
2016-07-11 16:30:43 +0800

    [16. 3Sum Closest][Accepted]committed by LeetCode Extension

--- 16-3Sum-Closest/solution.cpp
+++ 16-3Sum-Closest/solution.cpp
@@ -13,18 +13,21 @@ public:
         {
             if(i > 0 && nums[i] == nums[i-1]) continue;
             
-            for(int j = i+1; j < len-1; j++)
+            int j = i+1, k = len-1;
+            while(j<k)
             {
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
-                long tmp = nums[i]+nums[j];
-                for(int k = j+1; k < len; k++)
+                int cursum = nums[i]+nums[j]+nums[k];
+                if(abs(cursum-target) < minerr)
                 {
-                    if((k > j+1 && nums[k] == nums[k-1]) || (nums[k] < target - minerr - tmp)) continue;
-                    if(nums[k] > target + minerr - tmp) break;
-                    
-                    res = tmp + nums[k];
-                    minerr = abs(target - res);
+                    res = cursum;
+                    minerr = abs(target-res);
+                    if(minerr == 0) return res;
                 }
+                
+                if(cursum > target)
+                    k--;
+                else
+                    j++;
             }
         }
         
