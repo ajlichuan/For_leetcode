617b36a4da10ba9278453f5cc0e82f2af7da51a5
ajlichuan <ajlichuan@126.com>
2016-07-11 21:04:49 +0800

    [106. Construct Binary Tree from Inorder and Postorder Traversal][Accepted]committed by LeetCode Extension

--- 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.cpp
+++ 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.cpp
@@ -1,33 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
-private:
-    TreeNode* buildloop(vector<int>& in, vector<int>& po, int inst, int post, int len)
-    {
-        if(len < 1) return NULL;
-        
-        int rootval = po[post+len-1];
-        TreeNode* root = new TreeNode(rootval);
-        
-        int llen;
-        for(llen = 0; in[inst+llen] != rootval; llen++);
-        
-        root->left = buildloop(in, po, inst, post, llen);
-        root->right = buildloop(in, po, inst+llen+1, post+llen, len-1-llen);
-        
-        return root;
-    }
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        if(inorder.size() == 0)return NULL;
+        TreeNode *p;
+        TreeNode *root;
+        stack<TreeNode *> stn;
         
-        return buildloop(inorder, postorder, 0, 0, inorder.size());
+        root = new TreeNode(postorder.back()); 
+        stn.push(root); 
+        postorder.pop_back(); 
         
+        while(true)
+        {
+            if(inorder.back() == stn.top()->val) 
+            {
+                p = stn.top();
+                stn.pop(); 
+                inorder.pop_back(); 
+                if(inorder.size() == 0) break;
+                if(stn.size() && inorder.back() == stn.top()->val)
+                    continue;
+                p->left = new TreeNode(postorder.back()); 
+                postorder.pop_back();
+                stn.push(p->left);
+            }
+            else 
+            {
+                p = new TreeNode(postorder.back());
+                postorder.pop_back();
+                stn.top()->right = p; 
+                stn.push(p); 
+            }
+        }
+        return root;
     }
 };
